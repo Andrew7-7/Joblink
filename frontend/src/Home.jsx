3f9019dbb83adda9dff003aae1834a087cc334d2
axelkrnwn <axelkurn@gmail.com>
2025-03-25 15:39:17 +0700

    Feat: Add register form UI

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,8 +1,19 @@
+import { useContext } from "react"
+import AuthContext from "./AuthContext"
+import Register from "./Register"
+
 const Home = () => {
+
+    const {user} = useContext(AuthContext)
 
     return (
     <>
-        
+        {user == null ? 
+        <Register />
+        :
+        <>
+        </>
+        }
     </>
     )
 }
