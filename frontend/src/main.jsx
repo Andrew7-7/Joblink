757d793ab200fc49f19896e5dfd5cba6546bdc84
Charmender004 <widjajatjung.charles@gmail.com>
2025-03-28 22:20:46 +0700

    feed work experience

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import '../index.css';
 import {AuthProvider} from './AuthContext';
+import Feed from './feed';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <AuthProvider>
-        < App />
+        < Feed />
       </AuthProvider>
   </React.StrictMode>
 );
